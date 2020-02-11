5a3d8af1b4f4e5fe6c1a62019f4f803912e02052
Andrew Csoka <andy@andrewcsoka.com>
2020-02-11 14:26:25 +0000

    netwoirking warnings

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -3,7 +3,7 @@ The VPC
 ======*/
 
 resource "aws_vpc" "vpc" {
-  cidr_block           = "${var.vpc_cidr}"
+  cidr_block           = var.vpc_cidr
   enable_dns_hostnames = true
   enable_dns_support   = true
 
